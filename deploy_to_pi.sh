1bd3adb0948c8170c2087851c83957ea0cb3b331
Nate Prouty <nate@infonate.com>
2025-11-24 23:41:59 -0500

    Add requirements.txt, update deploy.

--- deploy_to_pi.sh
+++ deploy_to_pi.sh
@@ -4,14 +4,21 @@ PI_USER="prouty"
 PI_HOST="raspberrypi.local"
 REMOTE_DIR="/home/prouty/projects/noisedetector"
 
+# This rsync command syncs all files and folders in the current directory (./)
+# to the remote target, except for the files and directories explicitly excluded below.
 rsync -avz \
 	--exclude '.DS_Store' \
 	--exclude 'clips' \
 	--exclude 'training' \
 	--exclude 'old_events' \
 	--exclude 'recordings' \
+	--exclude 'baseline.json' \
 	--exclude 'events.csv' \
 	--exclude '__pycache__' \
-	./\
+	--exclude 'venv' \
+	--exclude '.git' \
+	./ \
 	"$PI_USER"@"$PI_HOST":"$REMOTE_DIR"
+
+
 
